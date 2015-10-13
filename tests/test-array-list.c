bfd3283a8fa7871b9a5eee3697a59b38b2c40fa4
Tristan Brindle <t.c.brindle@gmail.com>
2015-10-13 19:24:19 +1300

    Tests: Add a lot of ArrayList tests

--- tests/test-array-list.c
+++ tests/test-array-list.c
@@ -5,7 +5,7 @@
 
 #define TEST_TYPE_OBJECT (test_object_get_type())
 
-G_DECLARE_FINAL_TYPE (TestObject, test_object, TEST, OBJECT, GObject)
+G_DECLARE_FINAL_TYPE (TestObject, test_object, TEST, OBJECT, GInitiallyUnowned)
 
 struct _TestObject
 {
@@ -14,7 +14,7 @@ struct _TestObject
   int index;
 };
 
-G_DEFINE_TYPE (TestObject, test_object, G_TYPE_OBJECT)
+G_DEFINE_TYPE (TestObject, test_object, G_TYPE_INITIALLY_UNOWNED)
 
 static TestObject *
 test_object_new (int index)
@@ -34,34 +34,153 @@ test_object_init (TestObject *self)
 {
 }
 
+
 static void
-test_array_list (void)
+test_array_list_construct (void)
+{
+  GcArrayList *array_list = gc_array_list_new ();
+  g_assert_nonnull (array_list);
+  g_assert_true (gc_array_list_get_is_empty (array_list));
+  g_object_unref (array_list);
+
+  array_list = gc_array_list_new_with_element_type (TEST_TYPE_OBJECT);
+  g_assert_nonnull (array_list);
+  g_assert_true (gc_array_list_get_is_empty (array_list));
+  g_object_unref (array_list);
+
+  g_autoptr(GPtrArray) ptr_array = g_ptr_array_new_with_free_func (g_object_unref);
+  array_list = gc_array_list_new_from_ptr_array (ptr_array);
+  g_assert_nonnull (array_list);
+  g_assert_true (gc_array_list_get_is_empty (array_list));
+  g_object_unref (array_list);
+
+  array_list = gc_array_list_new_full (TEST_TYPE_OBJECT,
+                                       g_object_ref,
+                                       g_object_unref);
+  g_assert_nonnull (array_list);
+  g_assert_true (gc_array_list_get_is_empty (array_list));
+  g_object_unref (array_list);
+}
+
+static void
+test_array_list_append (void)
 {
   g_autoptr(GcArrayList) array_list = gc_array_list_new ();
-  g_assert_nonnull (array_list);
+  int i;
 
-  for (int i = 0; i < NUM_OBJECTS; i++)
+  for (i = 0; i < NUM_OBJECTS; i++)
     {
-      g_autoptr(TestObject) test_obj = test_object_new (i);
-      gc_array_list_append (array_list, test_obj);
+      gc_array_list_append (array_list, test_object_new (i));
     }
-
-  g_assert_cmpint (gc_array_list_get_size (array_list), ==, NUM_OBJECTS);
-
+  g_assert_false (gc_array_list_get_is_empty (array_list));
+  g_assert_cmpuint (gc_array_list_get_size (array_list), ==, NUM_OBJECTS);
 
   g_autoptr(GcIterator) iter = gc_iterable_iterator (GC_ITERABLE (array_list));
 
-  while (gc_iterator_next (iter) != 0) {
+  i = 0;
+  while (gc_iterator_next (iter)) {
       TestObject *t = TEST_OBJECT (gc_iterator_get (iter));
-      g_print ("%i\n", t->index);
+      g_assert_cmpint (t->index, ==, i++);
   }
+}
+
+static void
+test_array_list_clear (void)
+{
+  g_autoptr (GcArrayList) array_list = gc_array_list_new();
+  gc_array_list_append (array_list, test_object_new (0));
+  g_assert_false (gc_array_list_get_is_empty (array_list));
+  gc_array_list_clear (array_list);
+  g_assert_true (gc_array_list_get_is_empty (array_list));
+}
+
+static void
+test_array_list_prepend (void)
+{
+  int i;
+  g_autoptr(GcArrayList) array_list = gc_array_list_new ();
+
+  for (i = 0; i < NUM_OBJECTS; i++)
+    {
+      TestObject *test_obj = test_object_new (i);
+      gc_array_list_prepend (array_list, test_obj);
+    }
+  g_assert_cmpuint (gc_array_list_get_size (array_list), ==, NUM_OBJECTS);
+
+  g_autoptr(GcIterator) iter = gc_iterable_iterator (GC_ITERABLE (array_list));
+
+  i = NUM_OBJECTS;
+  while (gc_iterator_next (iter)) {
+      TestObject *t = TEST_OBJECT (gc_iterator_get (iter));
+      g_assert_cmpint (t->index, ==, --i);
+  }
+
+}
+
+static void
+test_array_list_get (void)
+{
+  g_autoptr(GcArrayList) array_list = gc_array_list_new ();
+
+  TestObject *in = test_object_new (-1);
+
+  gc_array_list_append (array_list, in);
+
+  TestObject *out = gc_array_list_get (array_list, 0);
+
+  g_assert (in == out);
+}
+
+static void
+test_array_list_set (void)
+{
+  g_autoptr(GcArrayList) array_list = gc_array_list_new ();
+  for (int i = 0; i < NUM_OBJECTS; i++)
+    {
+      gc_array_list_append (array_list, test_object_new (i));
+    }
+
+  TestObject *in = test_object_new (-1);
+  gc_array_list_set (array_list, 3, in);
+
+  TestObject *out = gc_array_list_get (array_list, 3);
+
+  g_assert (in == out);
+}
+
+static void
+test_array_list_insert (void)
+{
+  g_autoptr(GcArrayList) array_list = gc_array_list_new ();
+  for (int i = 0; i < NUM_OBJECTS; i++)
+    {
+      gc_array_list_append (array_list, test_object_new (i));
+    }
+
+
+  //g_autoptr(TestObject) in = test_object_new (-1);
+  TestObject *in = test_object_new (-1);
+  gc_array_list_insert (array_list, 3, in);
+
+  g_assert_cmpuint (gc_array_list_get_size (array_list), ==, NUM_OBJECTS + 1);
+
+  TestObject *out = gc_array_list_get (array_list, 3);
+
+  g_assert (in == out);
 }
 
 int
 main(int argc, char **argv)
 {
-   g_test_init(&argc, &argv, NULL);
-   g_test_add_func("/GCollection/ArrayList", test_array_list);
-   return g_test_run();
+  g_test_init(&argc, &argv, NULL);
+  g_test_add_func("/GCollection/ArrayList/Construct", test_array_list_construct);
+  g_test_add_func("/GCollection/ArrayList/Append", test_array_list_append);
+  g_test_add_func("/GCollection/ArrayList/Clear", test_array_list_clear);
+  g_test_add_func("/GCollection/ArrayList/Prepend", test_array_list_prepend);
+  g_test_add_func("/GCollection/ArrayList/Get", test_array_list_get);
+  g_test_add_func("/GCollection/ArrayList/Set", test_array_list_set);
+  g_test_add_func("/GCollection/ArrayList/Insert", test_array_list_insert);
+
+  return g_test_run();
 }
 
