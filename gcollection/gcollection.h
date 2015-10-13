f8a16c6374c3cf32fc563444e020cabbb81dcb00
Tristan Brindle <t.c.brindle@gmail.com>
2015-10-13 17:04:32 +1300

    Add gcollection.h
    Global header including everything else.
    
    Quite possibly this should go at the top level and everything else
    should go in a gcollection folder.

---
+++ gcollection/gcollection.h
@@ -0,0 +1,27 @@
+/* gcollection.h
+ *
+ * Copyright (C) 2015 Tristan Brindle <t.c.brindle@gmail.com>
+ *
+ * This file is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GCOLLECTION_H
+#define GCOLLECTION_H
+
+#include "gc-array-list.h"
+#include "gc-iterable.h"
+#include "gc-iterator.h"
+
+#endif /* GCOLLECTION_H */
+
